e409632215a49502a600758a2792bae043d17d0f
buseipek-ncrgl <ipekncrgl@gmail.com>
2025-08-21 16:02:13 +0300

    update

--- app/panel/layout.tsx
+++ app/panel/layout.tsx
@@ -77,15 +77,17 @@ export default function PanelLayout({ children }: { children: React.ReactNode })
         className={[
           "fixed inset-y-0 left-0 z-50 w-64 transition-transform duration-300 ease-in-out",
           open ? "translate-x-0" : "-translate-x-full lg:translate-x-0",
-          "lg:relative lg:translate-x-0 lg:flex lg:flex-col",
+          "lg:relative lg:translate-x-0 lg:flex lg:flex-col lg:h-full",
           "bg-gradient-to-b from-slate-50 via-white to-slate-50 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 border-r border-slate-200/60 dark:border-slate-700/60",
         ].join(" ")}
       >
-        <PanelSidebar onNavigate={() => setOpen(false)} />
+        <div className="flex h-full min-h-0 flex-col">
+          <PanelSidebar onNavigate={() => setOpen(false)} />
+        </div>
       </aside>
 
       {/* Sağ taraf */}
-      <div className="relative z-10 flex min-h-0 flex-col">
+      <div className="relative z-10 flex min-h-0 flex-col lg:h-full">
         {/* Topbar */}
         <PanelTopbar
           onMenuClick={() => setOpen((s) => !s)}
